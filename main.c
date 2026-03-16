25097fbe0d139fad0586f1d069567404eb6ffcc7
petar-romic-179-25 <petar.romic.179.25@ict.edu.rs>
2026-03-16 16:49:14 +0100

    Update main.c
    Ispisan program

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(){
+	int godina;
+	printf("Petar Romic \n");
+	printf("179/25 \n");
+	printf("Unesi godinu rodjenja: \n");
+	scanf("%d", &godina);
+	if(godina % 4 == 0){
+		printf("Rodjen si u prestupnoj godini \n");
+	} else{
+		printf("Nisi rodjen u prestupnoj godini \n"); 
+	}
 	return 0;
 }
